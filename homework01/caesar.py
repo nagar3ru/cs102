6f3cc3305c5b4c4bf74f8dfe005a703c79657b1c
nagar3ru <43826428+nagar3ru@users.noreply.github.com>
2018-10-19 22:52:48 +0300

    Update homework01/caesar.py

---
+++ homework01/caesar.py
@@ -0,0 +1,74 @@
+def encrypt_caesar(plaintext: str):
+
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+
+    ciphertext = [0] * len(plaintext)
+
+    case = {
+        'x': 'a',
+        'y': 'b',
+        'z': 'c',
+        'X': 'A',
+        'Y': 'B',
+        'Z': 'C'
+    }
+    for t in range(len(plaintext)):
+        ciphertext[t] = plaintext[t]
+    for i in range(len(plaintext)):
+        if 65 <= ord(plaintext[i]) <= 87 or 97 <= ord(plaintext[i]) <= 119:
+            ciphertext[i] = chr(ord(plaintext[i])+ 3)
+        elif 88 <= ord(plaintext[i]) <= 90 or 120 <= ord(plaintext[i]) <= 122:
+            ciphertext[i] = case[plaintext[i]]
+    ciphertext = ''.join(ciphertext)
+
+    return ciphertext
+
+
+print(encrypt_caesar(input()))
+
+
+
+def decrypt_caesar(ciphertext: str):
+
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+
+    plaintext = [0] * len(ciphertext)
+
+    case = {
+        'a': 'x',
+        'b': 'y',
+        'c': 'z',
+        'A': 'X',
+        'B': 'Y',
+        'C': 'Z'
+    }
+    for t in range(len(ciphertext)):
+        plaintext[t] = ciphertext[t]
+    for i in range(len(ciphertext)):
+        if ord('D') <= ord(ciphertext[i]) <= ord('Z') or ord('d') <= ord(ciphertext[i]) <= ord('z'):
+            plaintext[i] = chr(ord(ciphertext[i]) - 3)
+        elif 88 <= ord(ciphertext[i]) <= 90 or 120 <= ord(ciphertext[i]) <= 122:
+            plaintext[i] = case[ciphertext[i]]
+    plaintext = ''.join(plaintext)
+
+    return plaintext
+
+print(decrypt_caesar(input()))
