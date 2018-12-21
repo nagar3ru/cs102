37637a8fc21b8eb5cfa14bf88782761963ffdad6
nagar3ru <43826428+nagar3ru@users.noreply.github.com>
2018-12-21 18:53:39 +0300

    Update GameOfLife.py

--- homework03/GameOfLife.py
+++ homework03/GameOfLife.py
@@ -4,7 +4,9 @@ import random
 
 
 class GameOfLife:
-    def __init__(self, width=640, height=480, cell_size=10, speed=10):
+    """ Класс для работы с полем и выведением его на экран"""
+
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -21,63 +23,60 @@ class GameOfLife:
         # Скорость протекания игры
         self.speed = speed
 
-
-    def draw_grid(self):
-        # http://www.pygame.org/docs/ref/draw.html#pygame.draw.line
+    def draw_grid(self) -> None:
+        """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (x, 0), (x, self.height))
+            pygame.draw.line(self.screen, pygame.Color('black'), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (0, y), (self.width, y))
+            pygame.draw.line(self.screen, pygame.Color('black'), (0, y), (self.width, y))
 
-
-    def run(self):
+    def run(self) -> None:
+        """ Запустить игру """
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption('Game of Life')
         self.screen.fill(pygame.Color('white'))
+        clist = self.cell_list()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
+
+            self.draw_cell_list(clist)
             self.draw_grid()
+            clist = self.update_cell_list(clist)
+            # Отрисовка списка клеток
+            # Выполнение одного шага игры (обновление состояния ячеек)
+            # PUT YOUR CODE HERE
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
 
-
-    if __name__ == '__main__':
-        game = GameOfLife(320, 240, 20)
-        game.run()
-
-
     def cell_list(self, randomize: bool = True) -> list:
         """ Создание списка клеток.
-        :parameter randomize: Если параметр randomize = True, то создается список, где
-        каждая клетка может быть равновероятно живой или мертвой.
-        :return: Создание списка клеток.
+        :param randomize: Если True, то создается список клеток, где
+        каждая клетка равновероятно может быть живой (1) или мертвой (0).
+        :return: Список клеток, представленный в виде матрицы
         """
         if not randomize:
             self.clist = [[0 for col in range(self.cell_width)] for row in range(self.cell_height)]
         else:
             self.clist = [[random.randint(0, 1) for col in range(self.cell_width)] for row in range(self.cell_height)]
-            return self.clist
+        return self.clist
 
-
-     def draw_cell_list(self, clist: list) -> None:
-            """ Отображение списка клеток
-            :param rects: Отображение списка клеток 'rects' с закрашиванием их в
-        соответствующе цвета.
-            """
-            for rown, row in enumerate(self.clist):
-                for coln, col in enumerate(row):
-                    if col == 1:
-                        pygame.draw.rect(self.screen, pygame.Color('green'), (coln * self.cell_size, rown * self.cell_size, self.cell_size, self.cell_size))
-                    else:
-                        pygame.draw.rect(self.screen, pygame.Color('white'), (coln * self.cell_size, rown * self.cell_size, self.cell_size, self.cell_size))
-
+    def draw_cell_list(self, clist: list) -> None:
+        """ Отображение списка клеток
+        :param rects: Список клеток для отрисовки, представленный в виде матрицы
+        """
+        for rown, row in enumerate(self.clist):
+            for coln, col in enumerate(row):
+                if col == 1:
+                    pygame.draw.rect(self.screen, pygame.Color('green'),
+                                     (coln * self.cell_size, rown * self.cell_size, self.cell_size, self.cell_size))
+                else:
+                    pygame.draw.rect(self.screen, pygame.Color('white'),
+                                     (coln * self.cell_size, rown * self.cell_size, self.cell_size, self.cell_size))
 
     def get_neighbours(self, cell: tuple) -> list:
         """ Вернуть список соседей для указанной ячейки
